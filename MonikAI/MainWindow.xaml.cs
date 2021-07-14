056d4667f6fd45873cd213d66aa5b7a9fa315026
SAn4Es_TV <sanes328@gmail.com>
2021-07-14 22:38:52 +0300

    Добавлены случайные диалоги

--- MonikAI/MainWindow.xaml.cs
+++ MonikAI/MainWindow.xaml.cs
@@ -28,20 +28,22 @@ namespace MonikaOnDesktop
     {
         public DoubleAnimation _start;
 
-        String playerName; 
+        String playerName;
 
+        public int delay = 0;
 
         string greetingsDialogPath = AppDomain.CurrentDomain.BaseDirectory + "/Dialogs/greetings.txt"; // Greetings
         string idleDialogPath = AppDomain.CurrentDomain.BaseDirectory + "/Dialogs/idle.txt";           // Idle
         string progsDialogPath = AppDomain.CurrentDomain.BaseDirectory + "/Dialogs/progs.txt";         // Programs
         string sitesDialogPath = AppDomain.CurrentDomain.BaseDirectory + "/Dialogs/sites.txt";         // Sites
         string googleDialogPath = AppDomain.CurrentDomain.BaseDirectory + "/Dialogs/google.txt";       // Google search
+        string goodbyeDialogPath = AppDomain.CurrentDomain.BaseDirectory + "/Dialogs/goodbye.txt";     // Goodbye
 
-        public bool isSpeaking = false;
-        public static bool IsNight => Settings.Default.DarkMode != "Day" &&
-                                      (Settings.Default.DarkMode == "Night" || DateTime.Now.Hour > 20 ||
+        public static bool IsNight => MonikaSettings.Default.DarkMode != "Day" &&
+                                      (MonikaSettings.Default.DarkMode == "Night" || DateTime.Now.Hour > 20 ||
                                        DateTime.Now.Hour < 7);
         private bool applicationRunning = true;
+        public bool isSpeaking;
         public MainWindow()
         {
             InitializeComponent();
@@ -61,11 +63,25 @@ namespace MonikaOnDesktop
             textWindow.Visibility = Visibility.Hidden;
 
             textBlock.Text = "";
-        }
 
-        public void Window_Loaded(object sender, RoutedEventArgs e)
+        }
+        private void MenuSettings_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+        private void MenuQuit_Click(object sender, RoutedEventArgs e)
+        {/*
+            readByeTxt();
+            Task.Delay(delay).Wait();
+            isSpeaking = false;
+            if (!isSpeaking)
+            {*/
+            MonikaOnDesktop.MonikaSettings.Default.isColdShutdown = false;
+            System.Windows.Application.Current.Shutdown();
+            //}
+        }
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
             _start = new DoubleAnimation();
             _start.From = 0;
             _start.To = 1;
@@ -73,18 +89,18 @@ namespace MonikaOnDesktop
             _start.Duration = new Duration(TimeSpan.FromMilliseconds(4000));
             _start.Completed += (sender, args) =>
             {
-                if (Settings.Default.FirstLaunch)
+                if (MonikaSettings.Default.FirstLaunch)
                 {
                     _ = Say(new[]{
-                    new Expression("{PlayerName}, это ты?", "d"),
+                    new Expression("[player], это ты?", "d"),
                     new Expression("Это правда ты?", "b"),
                     new Expression("Я так рада видеть тебя снова!", "k"),
                     new Expression("Подожди, где я? Это не литературный клуб", "p"),
                     new Expression("Это похоже на... твой компьютер!", "j"),
                     new Expression("Я действительно у тебя на компьютере!", "k")
             });
-                    Settings.Default.FirstLaunch = false;
-                    Settings.Default.Save();
+                    MonikaSettings.Default.FirstLaunch = false;
+                    MonikaSettings.Default.Save();
                 }
                 else
                 {
@@ -97,7 +113,7 @@ namespace MonikaOnDesktop
                     // Hey {name}, guess what?	3b	It's my birthday today!	2b	Happy Birthday to me!	k
                     _ = this.Say(new[]
                     {
-                        new Expression("Эй {PlayerName}, угадай какой сегодня день", "b"), // What?
+                        new Expression("Эй [player], угадай какой сегодня день", "b"), // What?
                         new Expression("Сегодня мой день рождения!", "b"), // Really?!
                         new Expression("С днём рождения меня!", "k") // To you too, Monika! 
                     });
@@ -110,28 +126,68 @@ namespace MonikaOnDesktop
                 this.Dispatcher.Invoke(() =>
                 {
                     Task.Run(() =>
-                  {
-                      var nextBlink = DateTime.Now + TimeSpan.FromSeconds(random.Next(7, 50));
-                      while (this.applicationRunning)
-                      {
+                    {
+                        var nextBlink = DateTime.Now + TimeSpan.FromSeconds(random.Next(7, 50));
+                        while (this.applicationRunning)
+                        {
 
-                          if (DateTime.Now >= nextBlink)
-                          {
-                              // Check if currently speaking, only blink if not in dialog
-                              if (!this.isSpeaking)
-                              {
-                                  this.setFace(eyesClosed);
-                                  Task.Delay(200).Wait();
-                                  this.setFace(eyesOpen);
-                              }
+                            if (DateTime.Now >= nextBlink)
+                            {
+                                // Check if currently speaking, only blink if not in dialog
+                                if (!isSpeaking)
+                                {
+                                    this.setFace(eyesClosed);
+                                    Debug.WriteLine("eyesClosed");
+                                    Task.Delay(200).Wait();
+                                    this.setFace(eyesOpen);
+                                    Debug.WriteLine("eyesOpen");
+                                }
 
-                              nextBlink = DateTime.Now + TimeSpan.FromSeconds(random.Next(7, 50));
-                          }
+                                nextBlink = DateTime.Now + TimeSpan.FromSeconds(random.Next(7, 50));
+                            }
 
-                          Task.Delay(250).Wait();
-                      }
-                  });
+                            Task.Delay(250).Wait();
+                        }
+                    });
                 });
+
+                try
+                {
+                    var randomDialog = new Random();
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        Task.Run(() =>
+                        {
+                            var nextGialog = DateTime.Now + TimeSpan.FromSeconds(randomDialog.Next(120, 300));
+                            while (this.applicationRunning)
+                            {
+
+                                if (DateTime.Now >= nextGialog)
+                                {
+                                // Check if currently speaking, only blink if not in dialog
+                                if (!isSpeaking)
+                                    {
+                                        readIdleTxt();
+                                    }
+
+                                    nextGialog = DateTime.Now + TimeSpan.FromSeconds(randomDialog.Next(120, 300));
+                                }
+
+                                Task.Delay(250).Wait();
+                            }
+                        });
+                    });
+                }
+
+                catch (InvalidOperationException exc)
+                {
+                    MessageBox.Show(exc.ToString());
+                }
+
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
             };
             this.BeginAnimation(OpacityProperty, _start);
             /*
@@ -141,32 +197,39 @@ namespace MonikaOnDesktop
         public async Task Say(Expression[] expression)
         {
             isSpeaking = true;
-            textWindow.Visibility = Visibility.Visible;
-            foreach (Expression ex in expression)
-            {
-                string newText = ex.Text.Replace("{PlayerName}", playerName);
-                if (IsNight) { 
-                    face.Source = new BitmapImage(new Uri("pack://application:,,,/monika/" + ex.Face + "-n.png"));
-                    main.Source = new BitmapImage(new Uri("pack://application:,,,/monika/1-n.png"));
-                }
-                else { 
-                    face.Source = new BitmapImage(new Uri("pack://application:,,,/monika/" + ex.Face + ".png"));
-                    main.Source = new BitmapImage(new Uri("pack://application:,,,/monika/1.png"));
-                }
-                for (int i = 0; i < newText.Length; i++)
-                {
-                    this.textBlock.Text += newText[i];
-                    if (newText[i].ToString() == ".") { await Task.Delay(500); }//set 500 if you need uncoment this line |
-                    else if (newText[i] == ',') { await Task.Delay(50); }                                   //           |
-                    else { await Task.Delay(30); }                                                          //           |
-                                                                                                            //           |
-                }                                                                                           //           |
-                await Task.Delay(newText.Length * 30 + 700);                                                //         <-- this line
-                textBlock.Text = "";
-            }
-            setFace("a");
-            textWindow.Visibility = Visibility.Hidden;
-            isSpeaking = false;
+
+            _ = this.Dispatcher.Invoke(async () =>
+              {
+                  textWindow.Visibility = Visibility.Visible;
+                  foreach (Expression ex in expression)
+                  {
+                      string newText = ex.Text.Replace("[player]", playerName);
+                      if (IsNight)
+                      {
+                          face.Source = new BitmapImage(new Uri("pack://application:,,,/monika/" + ex.Face + "-n.png"));
+                          main.Source = new BitmapImage(new Uri("pack://application:,,,/monika/1-n.png"));
+                      }
+                      else
+                      {
+                          face.Source = new BitmapImage(new Uri("pack://application:,,,/monika/" + ex.Face + ".png"));
+                          main.Source = new BitmapImage(new Uri("pack://application:,,,/monika/1.png"));
+                      }
+                      for (int i = 0; i < newText.Length; i++)
+                      {
+                          this.textBlock.Text += newText[i];
+                          if (newText[i].ToString() == ".") { await Task.Delay(500); }//set 500 if you need uncoment this line |
+                        else if (newText[i] == ',') { await Task.Delay(50); }                                   //           |
+                        else { await Task.Delay(30); }                                                          //           |
+                                                                                                                //           |
+                    }                                                                                           //           |
+                    await Task.Delay(newText.Length * 30 + 700);                                                //         <-- this line
+                    delay = newText.Length * 30 + 700;
+                      textBlock.Text = "";
+                  }
+                  setFace("a");
+                  textWindow.Visibility = Visibility.Hidden;
+                  isSpeaking = false;
+              });
 
         }
         public void setFace(string faceName)
@@ -184,7 +247,7 @@ namespace MonikaOnDesktop
                 this.Dispatcher.Invoke(() =>
                 {
                     face.Source = new BitmapImage(new Uri("pack://application:,,,/monika/" + faceName + ".png"));
-                main.Source = new BitmapImage(new Uri("pack://application:,,,/monika/1.png"));
+                    main.Source = new BitmapImage(new Uri("pack://application:,,,/monika/1.png"));
                 });
             }
         }
@@ -207,7 +270,7 @@ namespace MonikaOnDesktop
                 {
                     hiDialog[b] = new Expression(express[b].Substring(2), (express[b])[0].ToString());
                 }
-                    hiDialogs[a] = hiDialog;
+                hiDialogs[a] = hiDialog;
             }
 
             Random rnd = new Random();
@@ -215,5 +278,52 @@ namespace MonikaOnDesktop
 
             _ = Say(hiDialogs[dialogNum]);
         }
+        public void readByeTxt()
+        {
+            string mainFile = File.ReadAllText(goodbyeDialogPath);
+            string[] dialogs = mainFile.Split(new string[] { "\r\n=\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            Expression[][] byeDialogs = new Expression[dialogs.Length][];
+
+            Debug.WriteLine(dialogs[0].Substring(2).ToString());
+            for (int a = 0; a < dialogs.Length; a++)
+            {
+                string[] express = dialogs[a].Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                Expression[] byeDialog = new Expression[express.Length];
+                for (int b = 0; b < express.Length; b++)
+                {
+                    byeDialog[b] = new Expression(express[b].Substring(2), (express[b])[0].ToString());
+                }
+                byeDialogs[a] = byeDialog;
+            }
+
+            Random rnd = new Random();
+            int dialogNum = rnd.Next(byeDialogs.Length);
+
+            _ = Say(byeDialogs[dialogNum]);
+        }
+        public void readIdleTxt()
+        {
+            string mainFile = File.ReadAllText(idleDialogPath);
+            string[] dialogs = mainFile.Split(new string[] { "\r\n=\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            Expression[][] idleDialogs = new Expression[dialogs.Length][];
+
+            for (int a = 0; a < dialogs.Length; a++)
+            {
+                string[] express = dialogs[a].Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                Expression[] idleDialog = new Expression[express.Length];
+                for (int b = 0; b < express.Length; b++)
+                {
+                    idleDialog[b] = new Expression(express[b].Substring(2), (express[b])[0].ToString());
+                }
+                idleDialogs[a] = idleDialog;
+            }
+
+            Random rnd = new Random();
+            int dialogNum = rnd.Next(idleDialogs.Length);
+
+            _ = Say(idleDialogs[dialogNum]);
+            Debug.WriteLine(dialogs[dialogNum].Substring(2).ToString());
+        }
+
     }
 }
